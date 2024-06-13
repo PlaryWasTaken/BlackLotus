ca40370f150ffb4a94ad31d3e5ab2c6f4a46eec2
PlaryWasTaken <70018287+PlaryWasTaken@users.noreply.github.com>
2024-06-13 19:52:16 -0300

    Fixes the remove.ts command to allow deleting Black Syndicate servers

--- slash_commands/embedConfig/remove.ts
+++ slash_commands/embedConfig/remove.ts
@@ -19,21 +19,38 @@ export default new Command({
         ),
     guilds: ["896047806454837278", "921162438001447023"],
     async run({client, interaction}) {
-        const id = interaction.options.getString('id')
-        await client.blackLotusManager.delete(id).then(async () => {
-            await interaction.reply({ephemeral: true, content: 'Servidor removido com sucesso'})
-        }).catch(async (err) => {
-            console.log(err)
-            await interaction.reply({ephemeral: false, content: `Não achei esse servidor ou ocorreu um erro. Esquisito...\nDe qualquer modo aqui está o erro:\n\`\`\`${err}\`\`\``})
-        })
+        const data = interaction.options.getString('id').split('-')
+        const id = data[0]
+        const isSyndicate = data[1] === 'true'
+        if (isSyndicate) {
+            await client.syndicateManager.delete(id).then(async () => {
+                await interaction.reply({ephemeral: true, content: 'Servidor removido com sucesso'})
+            }).catch(async (err) => {
+                console.log(err)
+                await interaction.reply({ephemeral: false, content: `Não achei esse servidor ou ocorreu um erro. Esquisito...\nDe qualquer modo aqui está o erro:\n\`\`\`${err}\`\`\``})
+            })
+        } else {
+            await client.blackLotusManager.delete(id).then(async () => {
+                await interaction.reply({ephemeral: true, content: 'Servidor removido com sucesso'})
+            }).catch(async (err) => {
+                console.log(err)
+                await interaction.reply({ephemeral: false, content: `Não achei esse servidor ou ocorreu um erro. Esquisito...\nDe qualquer modo aqui está o erro:\n\`\`\`${err}\`\`\``})
+            })
+        }
     },
-    async autocomplete({client, interaction}) {
-        const servers = await serverModel.find({ $or: [{ "blackLotus.displayName": new RegExp(interaction.options.getString('id'), 'i')}, {id: interaction.options.getString('id')}]}).limit(25)
+    async autocomplete({ interaction}) {
+        const servers = await serverModel.find({ $or: [
+                { "modules.blackLotus.displayName": new RegExp(interaction.options.getString('id'), 'i')},
+                { "modules.syndicate.displayName": new RegExp(interaction.options.getString('id'), 'i')},
+                {id: interaction.options.getString('id')},
+            ]}).limit(25)
         const options = []
         servers.forEach(server => {
+            if (!server.modules.blackLotus && !server.modules.syndicate) return
+            const isSyndicate = !server.modules.blackLotus
             options.push({
-                name: server.modules.blackLotus.displayName,
-                value: server.id
+                name: server.modules.blackLotus.displayName || server.modules.syndicate.displayName,
+                value: `${server.id}-${isSyndicate}`
             })
         })
         await interaction.respond(options)
