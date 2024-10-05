b43de598c486466019a3ca7c386dedb8cf8104ba
PlaryWasTaken <70018287+PlaryWasTaken@users.noreply.github.com>
2024-10-05 19:23:26 -0300

    Fixes tracking staff leaves, adds tracking for syndicate representants leaving

--- events/blackLotusUpdates/representantLeft.ts
+++ events/blackLotusUpdates/representantLeft.ts
@@ -8,25 +8,48 @@ import {EmbedBuilder, TextChannel} from "discord.js";
 
 export default new Event().setData("guildMemberRemove",async (client, member) => {
     if (member.guild.id === client.blackLotus.id) {
+        /**/
         const results = await client.blackLotusManager.fetchByKV({"modules.blackLotus.representant": member.id}).catch(() => {})
-        if (!results || results.length === 0) return
-        const embed = new EmbedBuilder()
-            .setTitle('Representante saiu')
-            .setDescription(`O representante ${member.displayName} (Id: ${member.id}) saiu do servidor`)
-            .setColor('#ff7f7f')
-            .setTimestamp()
-        await client.logChannel.send({ embeds: [embed] })
-        for (const guild of results) { // Delete all servers where the member is a representant
-            client.emit('blackLotus.guildRepresentantLeft', member, guild)
-            await guild.delete("Representante saiu do servidor da black lotus")
+        if (results && results.length > 0) {
+            const embed = new EmbedBuilder()
+                .setTitle('Representante saiu (Black)')
+                .setDescription(`O representante ${member.displayName} (Id: ${member.id}) saiu do servidor`)
+                .setColor('#ff7f7f')
+                .setTimestamp()
+            await client.logChannel.send({ embeds: [embed] })
+            for (const guild of results) { // Delete all servers where the member is a representant
+                client.emit('blackLotus.guildRepresentantLeft', member, guild)
+                await guild.delete("Representante saiu do servidor da black lotus")
+            }
         }
-
-        // Find all servers where the member is part of the staff team and remove him from the staffs array
-        const data = await client.blackLotusManager.fetchByKV({ "modules.blackLotus.staffs": {$elemMatch: {id: member.id}} })
-        if (!data) return
-        for (const server of data) {
-            await server.removeStaff(member.id)
+        /* BlackLotus Checks */
+        /* Syndicate Checks */
+        const syndicateResults = await client.syndicateManager.fetchByKV({"modules.syndicate.representant": member.id}).catch(() => {})
+        if (syndicateResults && syndicateResults.length > 0) {
+            const embed = new EmbedBuilder()
+                .setTitle('Representante saiu (Syndicate)')
+                .setDescription(`O representante ${member.displayName} (Id: ${member.id}) saiu do servidor`)
+                .setColor('#ff7f7f')
+                .setTimestamp()
+            await client.logChannel.send({ embeds: [embed] })
+            for (const guild of syndicateResults) { // Delete all servers where the member is a representant
+                client.emit('syndicate.guildRepresentantLeft', member, guild)
+                await guild.delete("Representante saiu do servidor da syndicate")
+            }
         }
-        client.emit('blackLotus.representantLeft', member)
+        /**/
+        // Remove staff from all servers
+        const blackLotusGuilds = await client.blackLotusManager.fetchByKV({ "modules.blackLotus.staffs": {$elemMatch: {id: member.id}} })
+        if (blackLotusGuilds) {
+            for (const server of blackLotusGuilds) {
+                await server.removeStaff(member.id)
+            }
+        }
+        const syndicateGuilds = await client.syndicateManager.fetchByKV({ "modules.syndicate.staffs": {$elemMatch: {id: member.id}} })
+        if (syndicateGuilds) {
+            for (const server of syndicateGuilds) {
+                await server.removeStaff(member.id)
+            }
+        }
     }
 })
