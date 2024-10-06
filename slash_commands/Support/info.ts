889f861b1ed4ab8836b22c29169e22795d021e78
PlaryWasTaken <70018287+PlaryWasTaken@users.noreply.github.com>
2024-10-06 14:21:56 -0300

    Should add capability for server info to be queried with /server for syndicate guilds

--- slash_commands/Support/info.ts
+++ slash_commands/Support/info.ts
@@ -47,19 +47,22 @@ export default new Command({
     switch (interaction.options.getSubcommand()) {
       case "info":
         const server = interaction.options.getString("server");
-        const guildData = await client.blackLotusManager.fetch(server).catch(() => {});
-        const guild = await client.guilds.fetch(server).catch(() => {});
-        if (!guildData || !guild)
+        const guildData = await client.blackLotusManager.fetch(server).catch(() => {}) || await client.syndicateManager.fetch(server).catch(() => {});
+        if (!guildData)
           return interaction.reply({ content: "Servidor não encontrado", ephemeral: true });
+        const guild = guildData.guild
         const owner = await client.users.fetch(guild.ownerId).catch(() => {});
         const rep = await client.users.fetch(guildData.representant);
+        let module = null;
+        if (guildData.data.modules.blackLotus.constellation) module = guildData.data.modules.blackLotus; // I can't just check for the module, since the module is always there, but only if the constellation is there its a black lotus server
+        else module = guildData.data.modules.syndicate; // If its not a black lotus server, its a syndicate server as the managers only return black lotus or syndicate servers
         const embed = new discord.EmbedBuilder()
-          .setTitle(`Informacões sobre o servidor ${guildData.data.modules.blackLotus.displayName}`)
+          .setTitle(`Informacões sobre o servidor ${module.displayName}`)
           .setFields([
             {
               name: "Informacões gerais",
               value: `Nome do servidor: ${guild.name}
-                                Nome de display: ${guildData.data.modules.blackLotus.displayName}
+                                Nome de display: ${module.displayName}
                                 Id: ${guild.id}
                                 Dono: ${owner ? owner.tag : guild.ownerId + " (Não encontrado)"}
                                 Representante: ${
@@ -72,9 +75,9 @@ export default new Command({
             {
               name: "Validacões",
               value: `Nome tem menos de 25 caracteres: ${
-                guildData.data.modules.blackLotus.displayName.length <= 25
-                  ? `Sim (${guildData.data.modules.blackLotus.displayName.length} Caracteres)`
-                  : `Não (${guildData.data.modules.blackLotus.displayName.length} Caracteres)`
+                module.displayName.length <= 25
+                  ? `Sim (${module.displayName.length} Caracteres)`
+                  : `Não (${module.displayName.length} Caracteres)`
               }
                                  Bot tem permissão para criar invites: ${
                                    guild.members.me.permissions.has(
@@ -95,13 +98,13 @@ export default new Command({
             {
               name: "Configuracões",
               value: `Segue crescimento: ${
-                guildData.data.modules.blackLotus.trackGrowth ? "Sim" : "Não"
+                module.trackGrowth ? "Sim" : "Não"
               }
                             Aparece na embed: ${
-                              guildData.data.modules.blackLotus.embedWorthy ? "Sim" : "Não"
+                              module.embedWorthy ? "Sim" : "Não"
                             }
                             Atualiza o nome automaticamente: ${
-                              guildData.data.modules.blackLotus.trackNameChanges ? "Sim" : "Não"
+                              module.trackNameChanges ? "Sim" : "Não"
                             }
                             `,
             },
@@ -111,7 +114,7 @@ export default new Command({
         break;
     }
   },
-  async autocomplete({ client, interaction }) {
+  async autocomplete({  interaction }) {
     const servers = await serverModel
       .find({
         $or: [
