1b2156c01b12e061b5eaba1bfc2a4eb70cc70048
PlaryWasTaken <70018287+PlaryWasTaken@users.noreply.github.com>
2024-10-06 14:13:17 -0300

    Fixes syndicate automatic name changes not being tracked and logged

--- events/blackLotusUpdates/guildNameUpdate.ts
+++ events/blackLotusUpdates/guildNameUpdate.ts
@@ -3,20 +3,28 @@ import Event from "../../classes/structs/Event";
 export default new Event().setData("guildUpdate", async (client, oldGuild, newGuild) => {
     if (oldGuild.name !== newGuild.name) {
         const guildData = await client.blackLotusManager.fetch(newGuild.id).catch(() => {})
-        if (!guildData || !guildData.data.modules.blackLotus.trackNameChanges) return
-
-
-
         const embed = new EmbedBuilder()
-            .setTitle('Nome do servidor alterado')
-            .setDescription(`O nome do servidor ${oldGuild.name} (Id: ${oldGuild.id}) foi alterado para ${newGuild.name}\n${newGuild.name.length > 25?'Também foi desativado o embedWorthy, pois o nome do servidor é maior que 25 caracteres': guildData.data.modules.blackLotus.embedWorthy?'':'O embedWorthy foi ativado novamente'}`)
-            .setColor('#7700ff')
-        await guildData.updateName(newGuild.name)
-        if (guildData.serverRoleId) {
-            const role = await client.blackLotus.roles.fetch(guildData.serverRoleId).catch(() => {})
-            if (role) {
-                await role.edit({ name: newGuild.name })
+
+        if (guildData && guildData.data.modules.blackLotus.trackNameChanges) {
+            embed
+                .setTitle('Nome do servidor alterado (Black)')
+                .setDescription(`O nome do servidor ${oldGuild.name} (Id: ${oldGuild.id}) foi alterado para ${newGuild.name}\n${newGuild.name.length > 25?'Também foi desativado o embedWorthy, pois o nome do servidor é maior que 25 caracteres': guildData.data.modules.blackLotus.embedWorthy?'':'O embedWorthy foi ativado novamente'}`)
+                .setColor('#7700ff')
+            await guildData.updateName(newGuild.name)
+            if (guildData.serverRoleId) {
+                const role = await client.blackLotus.roles.fetch(guildData.serverRoleId).catch(() => {})
+                if (role) {
+                    await role.edit({ name: newGuild.name })
+                }
             }
+        }
+        const syndicateData = await client.syndicateManager.fetch(newGuild.id).catch(() => {})
+        if (syndicateData && syndicateData.data.modules.syndicate.trackNameChanges) {
+            embed
+                .setTitle('Nome do servidor alterado (Syndicate)')
+                .setDescription(`O nome do servidor ${oldGuild.name} (Id: ${oldGuild.id}) foi alterado para ${newGuild.name}\n${newGuild.name.length > 25?'Também foi desativado o embedWorthy, pois o nome do servidor é maior que 25 caracteres': syndicateData.data.modules.syndicate.embedWorthy?'':'O embedWorthy foi ativado novamente'}`)
+                .setColor('#7700ff')
+            await syndicateData.updateName(newGuild.name)
         }
         await client.logChannel.send({embeds: [embed]})
     }
