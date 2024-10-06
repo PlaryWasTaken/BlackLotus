3b19942fab143c4ae9434bab8a7f6d499723ec79
PlaryWasTaken <70018287+PlaryWasTaken@users.noreply.github.com>
2024-10-06 14:55:13 -0300

    Uh

--- events/interactionTypes/modals/partner/messageChange.ts
+++ events/interactionTypes/modals/partner/messageChange.ts
@@ -1,7 +1,8 @@
 import Event from "#structs/Event";
 
 export default new Event().setData("modal.messageChange", async (client, interaction) => {
-    const server = await client.blackLotusManager.fetch(interaction.guild.id)
+    const server = await client.blackLotusManager.fetch(interaction.guild.id).catch(() => {}) || await client.syndicateManager.fetch(interaction.guild.id).catch(() => {})
+    if (!server) return interaction.reply({ ephemeral: true, content: `Ocorreu um erro ao tentar encontrar o servidor.`})
     server.data.modules.partnerships.message = interaction.fields.getTextInputValue('messageInput')
     await server.data.save()
     await interaction.reply({ ephemeral: true, content: `Troquei a sua mensagem de parceria com sucesso!`})
